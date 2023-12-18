4b273f8c5fb00a15a76add53d4e694ab52e4ef97
ErickUrbizagastegui <u20201e465@upc.edu.pe>
2023-12-18 15:47:38 -0500

    Creado el BaseRepository

---
+++ Shared/Persistence/Respositories/BaseRepository.cs
@@ -0,0 +1,13 @@
+﻿using JuegoA_API.Shared.Persistence.Contexts;
+
+namespace JuegoA_API.Shared.Persistence.Respositories;
+
+public class BaseRepository
+{
+    protected readonly AppDbContext _context;
+
+    public BaseRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+}
