6c98afb7f0444da9b493b450f49279997dfed732
ErickUrbizagastegui <u20201e465@upc.edu.pe>
2023-12-18 16:30:07 -0500

    Creados los controllers para Jugadores y Personajes.

---
+++ Juego-A/Controllers/JugadoresController.cs
@@ -0,0 +1,78 @@
+﻿using AutoMapper;
+using JuegoA_API.Juego_A.Domain.Models;
+using JuegoA_API.Juego_A.Domain.Services;
+using JuegoA_API.Juego_A.Resources;
+using JuegoA_API.Shared.Extensions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JuegoA_API.Juego_A.Controllers;
+
+[ApiController]
+[Route("api/v1/[controller]")]
+public class JugadoresController : ControllerBase
+{
+    private readonly IJugadorService _jugadorService;
+    private readonly IMapper _mapper;
+
+    public JugadoresController(IJugadorService jugadorService, IMapper mapper)
+    {
+        _jugadorService = jugadorService;
+        _mapper = mapper;
+    }
+    
+    [HttpGet]
+    public async Task<IEnumerable<JugadorResource>> GetAllAsync()
+    {
+        var jugadores = await _jugadorService.ListAsync();
+        var resources = _mapper.Map<IEnumerable<Jugador>, 
+            IEnumerable<JugadorResource>>(jugadores);
+        return resources;
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> PostAsync([FromBody] SaveJugadorResource resource)
+    {
+        if (!ModelState.IsValid)
+            return BadRequest(ModelState.GetErrorMessages());
+        
+        var jugador = _mapper.Map<SaveJugadorResource, Jugador>(resource);
+        var result = await _jugadorService.SaveAsync(jugador);
+        
+        if (!result.Success)
+            return BadRequest(result.Message);
+        
+        var jugadorResource = _mapper.Map<Jugador, JugadorResource>(result.Resource);
+        
+        return Ok(jugadorResource);
+    }
+    
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutAsync(int id, [FromBody] SaveJugadorResource resource)
+    {
+        if (!ModelState.IsValid)
+            return BadRequest(ModelState.GetErrorMessages());
+ 
+        var jugador = _mapper.Map<SaveJugadorResource, Jugador>(resource);
+        var result = await _jugadorService.UpdateAsync(id, jugador);
+ 
+        if (!result.Success)
+            return BadRequest(result.Message);
+        
+        var jugadorResource = _mapper.Map<Jugador, JugadorResource>(result.Resource);
+        
+        return Ok(jugadorResource);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteAsync(int id)
+    {
+        var result = await _jugadorService.DeleteAsync(id);
+        
+        if (!result.Success)
+            return BadRequest(result.Message);
+ 
+        var jugadorResource = _mapper.Map<Jugador, JugadorResource>(result.Resource);
+        
+        return Ok(jugadorResource);
+    }
+}
---
+++ Juego-A/Controllers/PersonajesController.cs
@@ -0,0 +1,77 @@
+﻿using AutoMapper;
+using JuegoA_API.Juego_A.Domain.Models;
+using JuegoA_API.Juego_A.Domain.Services;
+using JuegoA_API.Juego_A.Resources;
+using JuegoA_API.Shared.Extensions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JuegoA_API.Juego_A.Controllers;
+
+[ApiController]
+[Route("api/v1/[controller]")]
+public class PersonajesController : ControllerBase
+{
+    private readonly IPersonajeService _personajeService;
+    private readonly IMapper _mapper;
+
+    public PersonajesController(IPersonajeService personajeService, IMapper mapper)
+    {
+        _personajeService = personajeService;
+        _mapper = mapper;
+    }
+
+    [HttpGet]
+    public async Task<IEnumerable<PersonajeResource>> GetAllAsync()
+    {
+        var personajes = await _personajeService.ListAsync();
+        var resources = _mapper.Map<IEnumerable<Personaje>, IEnumerable<PersonajeResource>>(personajes);
+        return resources;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> PostAsync([FromBody] SavePersonajeResource resource)
+    {
+        if (!ModelState.IsValid)
+            return BadRequest(ModelState.GetErrorMessages());
+
+        var personaje = _mapper.Map<SavePersonajeResource, Personaje>(resource);
+        var result = await _personajeService.SaveAsync(personaje);
+
+        if (!result.Success)
+            return BadRequest(result.Message);
+
+        var personajeResource = _mapper.Map<Personaje, PersonajeResource>(result.Resource);
+
+        return Ok(personajeResource);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutAsync(int id, [FromBody] SavePersonajeResource resource)
+    {
+        if (!ModelState.IsValid)
+            return BadRequest(ModelState.GetErrorMessages());
+
+        var personaje = _mapper.Map<SavePersonajeResource, Personaje>(resource);
+        var result = await _personajeService.UpdateAsync(id, personaje);
+
+        if (!result.Success)
+            return BadRequest(result.Message);
+
+        var personajeResource = _mapper.Map<Personaje, PersonajeResource>(result.Resource);
+
+        return Ok(personajeResource);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteAsync(int id)
+    {
+        var result = await _personajeService.DeleteAsync(id);
+
+        if (!result.Success)
+            return BadRequest(result.Message);
+
+        var personajeResource = _mapper.Map<Personaje, PersonajeResource>(result.Resource);
+
+        return Ok(personajeResource);
+    }
+}
