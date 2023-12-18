40800910127b610dd515631eadc05ed1beb4ff41
ErickUrbizagastegui <u20201e465@upc.edu.pe>
2023-12-18 15:01:38 -0500

    Creadas las interfaces de los services

---
+++ Juego-A/Domain/Services/IJugadorService.cs
@@ -0,0 +1,12 @@
+﻿using JuegoA_API.Juego_A.Domain.Models;
+using JuegoA_API.Juego_A.Domain.Services.Communication;
+
+namespace JuegoA_API.Juego_A.Domain.Services;
+
+public interface IJugadorService
+{
+    Task<IEnumerable<Jugador>> ListAsync();
+    Task<JugadorResponse> SaveAsync(Jugador jugador);
+    Task<JugadorResponse> UpdateAsync(int id, Jugador jugador);
+    Task<JugadorResponse> DeleteAsync(int id);
+}
---
+++ Juego-A/Domain/Services/IPersonajeService.cs
@@ -0,0 +1,12 @@
+﻿using JuegoA_API.Juego_A.Domain.Models;
+using JuegoA_API.Juego_A.Domain.Services.Communication;
+
+namespace JuegoA_API.Juego_A.Domain.Services;
+
+public interface IPersonajeService
+{
+    Task<IEnumerable<Personaje>> ListAsync();
+    Task<PersonajeResponse> SaveAsync(Personaje personaje);
+    Task<PersonajeResponse> UpdateAsync(int id, Personaje personaje);
+    Task<PersonajeResponse> DeleteAsync(int id);
+}
