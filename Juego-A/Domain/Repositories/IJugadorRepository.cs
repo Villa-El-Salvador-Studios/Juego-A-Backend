9abf86f2d56b290773d1ca87c2bf05e51fade67c
ErickUrbizagastegui <u20201e465@upc.edu.pe>
2023-12-18 15:28:48 -0500

    Creadas las interfaces de los repositorios base de los entities.

---
+++ Juego-A/Domain/Repositories/IJugadorRepository.cs
@@ -0,0 +1,11 @@
+﻿using JuegoA_API.Juego_A.Domain.Models;
+
+namespace JuegoA_API.Juego_A.Domain.Repositories;
+
+public interface IJugadorRepository
+{
+    Task<IEnumerable<Jugador>> ListAsync();
+    Task AddAsync(Jugador jugador);
+    void Update(Jugador jugador);
+    void Remove(Jugador jugador);
+}
---
+++ Juego-A/Domain/Repositories/IPersonajeRepository.cs
@@ -0,0 +1,11 @@
+﻿using JuegoA_API.Juego_A.Domain.Models;
+
+namespace JuegoA_API.Juego_A.Domain.Repositories;
+
+public interface IPersonajeRepository
+{
+    Task<IEnumerable<Personaje>> ListAsync();
+    Task AddAsync(Personaje personaje);
+    void Update(Personaje personaje);
+    void Remove(Personaje personaje);
+}
