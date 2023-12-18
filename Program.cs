d5fd2578a793b6e3bd8f60953dc9fd7be0ccb01a
ErickUrbizagastegui <u20201e465@upc.edu.pe>
2023-12-18 16:47:09 -0500

    Configurado el Program.cs para que funcione el API.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
+using JuegoA_API.Juego_A.Domain.Repositories;
+using JuegoA_API.Juego_A.Domain.Services;
+using JuegoA_API.Juego_A.Mapping;
+using JuegoA_API.Juego_A.Persistence.Repositories;
+using JuegoA_API.Juego_A.Services;
+using JuegoA_API.Shared.Persistence.Contexts;
+using JuegoA_API.Shared.Persistence.Respositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,7 +16,37 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Add Database Connection
+var connectionString = 
+    builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(
+    options => options.UseMySQL(connectionString)
+        .LogTo(Console.WriteLine, LogLevel.Information)
+        .EnableSensitiveDataLogging()
+        .EnableDetailedErrors());
+
+// Add lowercase routes
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
+
+// Dependency Injection Configuration
+builder.Services.AddScoped<IJugadorRepository, JugadorRepository>();
+builder.Services.AddScoped<IJugadorService, JugadorService>();
+builder.Services.AddScoped<IPersonajeRepository, PersonajeRepository>();
+builder.Services.AddScoped<IPersonajeService, PersonajeService>();
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+// AutoMapper Configuration
+builder.Services.AddAutoMapper(
+    typeof(ModelToResourceProfile), 
+    typeof(ResourceToModelProfile));
+
 var app = builder.Build();
+
+// Validation for ensuring Database Objects are created
+using (var scope = app.Services.CreateScope())
+using (var context = scope.ServiceProvider.GetService<AppDbContext>())
+{
+    context.Database.EnsureCreated();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
