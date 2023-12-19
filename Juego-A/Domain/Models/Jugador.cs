0d1b5eaab7564ee5dd7481027e146fdab0256e5c
EUrbizagastegui <erickurbizagastegui@hotmail.com>
2023-12-19 13:31:19 -0500

    Modificado el entity jugador, para incluir los cambios de Mundo.

--- Juego-A/Domain/Models/Jugador.cs
+++ Juego-A/Domain/Models/Jugador.cs
@@ -6,7 +6,10 @@ public class Jugador
     public string Usuario { get; set; }
     public string Contrasenia { get; set; }
     public string fotoPerfil { get; set; }
+    public int MundoMaximo { get; set; }
 
     // Relaciones
     public IList<Personaje> Personajes = new List<Personaje>();
+    public int Mundo_Id { get; set; }
+    public Mundo Mundo { get; set; }
 }
