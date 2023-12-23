6ca0f87f8477f2cae585776e0dc905bc493e3f5b
EUrbizagastegui <erickurbizagastegui@hotmail.com>
2023-12-23 12:10:06 -0500

    Creado el resource para verificar si el usuario ya existe en la base de datos.

---
+++ Juego-A/Resources/VerifyUserResource.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JuegoA_API.Juego_A.Resources;
+
+public class VerifyUserResource
+{
+    [Required]
+    public string Usuario { get; set; }
+}
