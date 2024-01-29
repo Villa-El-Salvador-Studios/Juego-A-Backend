15fb1ac96ed57f9d364ced9e4a07632eae3b2db6
EUrbizagastegui <erickurbizagastegui@hotmail.com>
2024-01-29 13:06:59 -0500

    Corregido error en los resources de objeto.

--- Juego-A/Resources/ObjetoResource.cs
+++ Juego-A/Resources/ObjetoResource.cs
@@ -5,9 +5,5 @@ public class ObjetoResource
     public int Id { get; set; }
     public string Nombre { get; set; }
     public string Descripcion { get; set; }
-    public int Cantidad { get; set; }
     public string Imagen { get; set; }
-    
-    // Relaciones
-    public int? jugadorId { get; set; }
 }
--- Juego-A/Resources/SaveObjetoResource.cs
+++ Juego-A/Resources/SaveObjetoResource.cs
@@ -4,9 +4,5 @@ public class SaveObjetoResource
 {
     public string Nombre { get; set; }
     public string Descripcion { get; set; }
-    public int Cantidad { get; set; }
     public string Imagen { get; set; }
-    
-    // Relaciones
-    public int? jugadorId { get; set; }
 }
